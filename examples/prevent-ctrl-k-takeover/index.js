03de97b1e9e709d58559724e2d9961f1e4545bea
Eric King <eric@webdeveric.com>
2021-05-20 16:02:40 -0500

    Initial work on ctrl-k takeover prevention

---
+++ examples/prevent-ctrl-k-takeover/index.js
@@ -0,0 +1,15 @@
+document.addEventListener(
+  'keydown',
+  event => {
+    if (
+      event.code === 'KeyK' &&
+      (event.getModifierState('Control') || event.getModifierState('Meta'))
+    ) {
+      event.stopPropagation();
+    }
+  },
+  {
+    capture: true,
+    passive: true,
+  },
+);
